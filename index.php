b8d52b713620dfeb86304b5dd85a4b1fc113df61
KACofficial <120970850+KACofficial@users.noreply.github.com>
2023-06-04 20:28:38 -0500

    Update index.php

--- index.php
+++ index.php
@@ -31,30 +31,53 @@
   <h1>File Sharing Website</h1>
 
   <?php
+  require_once 'config.php';
 
-  ini_set('post_max_size', '0');
-  ini_set('upload_max_filesize', '0');
-  ini_set('max_execution_time', '0');
+  // Function to log user information
+  function logUpload($ip, $device, $fileName)
+  {
+    $logEntry = "IP: " . $ip . " | Device: " . $device . " | File: " . $fileName . "\n";
+    file_put_contents(UPLOAD_LOGS, $logEntry, FILE_APPEND);
+  }
+
+  // Function to block certain IP addresses
+  function blockIP($ip)
+  {
+    $blockedIPs = ['']; // Add IP addresses to block here
+
+    if (in_array($ip, $blockedIPs)) {
+      echo '<p>Uploads from your IP address are blocked.</p>';
+      exit;
+    }
+  }
 
   if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['file'])) {
     $file = $_FILES['file'];
     $originalFileName = $file['name'];
     $fileExtension = strtolower(pathinfo($originalFileName, PATHINFO_EXTENSION));
-    $fileName = str_replace(' ', '-', $_POST['filename']) . '.' . $fileExtension; // Convert spaces to dashes and append the original file extension
+    
+    // Check if the file extension is in the list of programming languages
+    $programmingLanguages = ['html', 'js', 'php', 'py', 'java', 'cpp', 'xml']; // Add more if needed
+    if (in_array($fileExtension, $programmingLanguages)) {
+      $fileExtension = 'txt'; // Change the extension to .txt
+    }
+    
+    $fileName = str_replace(' ', '-', $_POST['filename']) . '.' . $fileExtension;
 
-    // Check for errors during file upload
     if ($file['error'] === UPLOAD_ERR_OK) {
-      $uploadPath = 'uploads/' . $fileName; // Use the modified file name for the upload path
 
-      // Move the uploaded file to the uploads directory with the new file name
+      $uploadPath = 'uploads/' . $fileName;
+
       if (move_uploaded_file($file['tmp_name'], $uploadPath)) {
-        // File uploaded successfully
         echo '<p>File uploaded successfully.</p>';
 
-        // Send file details to Discord webhook
-        $discordWebhookURL = 'https://discord.com/api/webhooks/1107841371747909642/cH6Vs0ynBt9V1IDdGmd8ijhHJGqgoYGVSc8BzT3DwTl4JK5txLMWfh7-SlCkQ1a9deNi';
+        // Log user information
+        $ip = $_SERVER['REMOTE_ADDR'];
+        $userAgent = $_SERVER['HTTP_USER_AGENT'];
+        logUpload($ip, $userAgent, $fileName);
 
-        $discordMessage = 'New file uploaded: ' . $fileName . "\n\n" . 'Download link: [Download](https://vanceperrymadethis.mrcoolblox.repl.co/download.php?file=' . $fileName . ')' . "\n" . 'View it here: [view](https://vanceperrymadethis.mrcoolblox.repl.co)';
+        $discordWebhookURL = DISCORD_WEBHOOK_URL;
+        $discordMessage = 'New file uploaded: ' . $fileName . "\n\n" . 'Download link: [Download](https://vanceperrymadethis.mrcoolblox.repl.co/download.php?file=' . $fileName . ')' . "\n" . 'View it here (only works for some files): [View](https://vanceperrymadethis.mrcoolblox.repl.co/uploads/' . $fileName . ')';
 
         $payload = json_encode([
           'content' => $discordMessage
@@ -74,29 +97,29 @@
 
         exit;
       } else {
-        // Failed to move uploaded file
         echo '<p>Failed to move uploaded file.</p>';
       }
     } else {
-      // Error during file upload
       echo '<p>Error during file upload.</p>';
     }
   }
+
+  // Check and block certain IP addresses
+  $ip = $_SERVER['REMOTE_ADDR'];
+  blockIP($ip);
   ?>
 
   <form id="upload-form" action="index.php" method="post" enctype="multipart/form-data">
-    <input type="file" name="file" id="file-input" />
+    <input type="file" name="file" id="file-input" required />
     <label for="filename-input">File Name:</label>
-    <input type="text" name="filename" id="filename-input" />
-    <input type="submit" value="Upload" id="upload-button" />
+    <input type="text" name="filename" id="filename-input" required />
+    <button type="submit" id="upload-button">Upload</button>
   </form>
 
   <div id="progress-bar">
     <div id="progress"></div>
   </div>
-
   <hr>
-
   <h2>Shared Files</h2>
   <ul>
     <?php
@@ -104,22 +127,22 @@
     foreach ($uploadedFiles as $file) {
       $fileName = basename($file);
       $fileExtension = strtolower(pathinfo($file, PATHINFO_EXTENSION));
-      echo '<li>' . $fileName;
+      echo '<li>' . htmlspecialchars($fileName);
 
-      // Display preview for supported file types
       if (in_array($fileExtension, ['jpg', 'jpeg', 'png', 'gif'])) {
-        echo '<br><img src="' . $file . '" width="300" height="auto" />';
+        echo '<br><img src="' . htmlspecialchars($file) . '" width="300" height="auto" />';
       } elseif (in_array($fileExtension, ['mp4', 'webm', 'ogg', 'mov'])) {
-        echo '<br><video src="' . $file . '" width="400" height="300" controls></video>';
+        echo '<br><video src="' . htmlspecialchars($file) . '" width="400" height="300" controls></video>';
       } elseif (in_array($fileExtension, ['pdf'])) {
-        echo '<br><embed src="' . $file . '" width="500" height="700" type="application/pdf">';
+        echo '<br><embed src="' . htmlspecialchars($file) . '" width="500" height="700" type="application/pdf">';
+      } elseif (in_array($fileExtension, ['mp3', 'wav', 'ogg', 'm4a'])) {
+        echo '<br><audio src="' . htmlspecialchars($file) . '" controls></audio>';
       }
 
       echo '<a href="download.php?file=' . urlencode($fileName) . '" class="download-button">Download</a></li>';
     }
     ?>
   </ul>
-
   <script>
     document.getElementById('upload-form').addEventListener('submit', function(e) {
       e.preventDefault();
@@ -127,7 +150,7 @@
       var fileInput = document.getElementById('file-input');
       var file = fileInput.files[0];
       var filenameInput = document.getElementById('filename-input');
-      var filename = filenameInput.value.trim(); // Get the value of the filename input
+      var filename = filenameInput.value.trim();
 
       if (filename === "") {
         alert("Please enter a file name.");
@@ -139,12 +162,11 @@
 
       var formData = new FormData();
       formData.append('file', file);
-      formData.append('filename', sanitizedFilename); // Include the desired file name in the form data
+      formData.append('filename', sanitizedFilename);
 
       var xhr = new XMLHttpRequest();
       xhr.open('POST', 'index.php', true);
 
-      // Track upload progress
       xhr.upload.addEventListener('progress', function(event) {
         if (event.lengthComputable) {
           var progressPercent = (event.loaded / event.total) * 100;
@@ -153,37 +175,17 @@
         }
       });
 
-      // Handle upload completion
       xhr.onload = function() {
         if (xhr.status === 200) {
-          // File uploaded successfully
           console.log('File uploaded successfully.');
-          window.location.reload(); // Refresh the page after upload
+          window.location.reload();
         } else {
-          // Failed to upload file
           console.log('Failed to upload file.');
         }
       };
 
       xhr.send(formData);
     });
-
-    // Initiate file download
-    <?php
-    if (isset($_GET['file'])) {
-      $fileName = basename($_GET['file']);
-      $filePath = 'uploads/' . $fileName;
-
-      if (file_exists($filePath)) {
-        header('Content-Type: application/octet-stream');
-        header('Content-Disposition: attachment; filename="' . $fileName . '"');
-        readfile($filePath);
-        exit;
-      } else {
-        echo 'File not found.';
-      }
-    }
-    ?>
   </script>
 </body>
 </html>
